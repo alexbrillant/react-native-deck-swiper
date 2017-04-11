b6bd5565a44e40959ac55b55bf8b585360de3fda
alexbrillant <abrillant23@gmail.com>
2017-04-11 01:26:11 -0400

    added Exemple

---
+++ Exemple/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import Swiper from 'react-native-deck-swiper'
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native'
+
+export default class Exemple extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards:  ['1', '2', '3'],
+      swipedAllCards: false
+    }
+  }
+
+  renderCard = (card) => {
+    return (
+      <View style={styles.card}>
+        <Text style={styles.text}>{card}</Text>
+      </View>
+    )
+  }
+
+  onSwipedAllCards = () => {
+    this.setState({
+      swipedAllCards: true
+    })
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Swiper
+          cards={this.state.cards}
+          renderCard={this.renderCard}
+          onSwipedAll={this.onSwipedAllCards}
+        />
+        {
+          this.state.swipedAllCards &&
+          <Text style={styles.done}>Lesson done!</Text>
+        }
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  card: {
+    flex: 1,
+    alignSelf: 'stretch',
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: '#E8E8E8',
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 50,
+    backgroundColor: 'transparent'
+  },
+  done: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: 'white',
+    backgroundColor: 'transparent'
+  }
+})
