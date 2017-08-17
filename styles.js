aa918d8d3cbbcebfb7d183cf0fb6accf5bf900c2
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2017-08-17 15:16:31 -0500

    format project to standard spec that was used previously, but now enforce it with prettier

--- styles.js
+++ styles.js
@@ -7,36 +7,36 @@ const styles = StyleSheet.create({
   },
   container: {
     alignItems: "stretch",
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     right: 0,
     bottom: 0
   },
   childrenViewStyle: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     right: 0,
     bottom: 0
   },
   overlayLabelWrapper: {
-    position: 'absolute',
-    backgroundColor: 'transparent',
+    position: "absolute",
+    backgroundColor: "transparent",
     zIndex: 2,
     flex: 1,
-    width: '100%',
-    height: '100%'
+    width: "100%",
+    height: "100%"
   },
   hideOverlayLabel: {
     opacity: 0
   },
   overlayLabel: {
     fontSize: 45,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     borderRadius: 10,
     padding: 10,
-    overflow: 'hidden'
+    overflow: "hidden"
   }
 });
 
