62bb5e1a56270fff34aa1eb8b854f084a198d7a5
Yunus Emre - Mac <yunusemre@sariilanlar.com>
2020-06-09 00:39:15 +0300

    added default native driver values
    (cherry picked from commit fb9fbd6283f4144889aef8e98f9e765fc74b37de)

--- Swiper.js
+++ Swiper.js
@@ -204,8 +204,9 @@ class Swiper extends Component {
         slideGesture: true
       })
     }
-
-    return Animated.event([null, this.createAnimatedEvent()])(
+    return Animated.event([null, this.createAnimatedEvent()], {
+      useNativeDriver: false
+    })(
       event,
       gestureState
     )
@@ -352,6 +353,7 @@ class Swiper extends Component {
 
   resetTopCard = cb => {
     Animated.spring(this.state.pan, {
+      useNativeDriver: false,
       toValue: 0,
       friction: this.props.topCardResetAnimationFriction,
       tension: this.props.topCardResetAnimationTension
@@ -422,6 +424,7 @@ class Swiper extends Component {
     this.setState({ panResponderLocked: true })
     this.animateStack()
     Animated.timing(this.state.pan, {
+      useNativeDriver: false,
       toValue: {
         x: x * SWIPE_MULTIPLY_FACTOR,
         y: y * SWIPE_MULTIPLY_FACTOR
@@ -524,7 +527,7 @@ class Swiper extends Component {
           swipedAllCards = true
         }
       } else {
-        newCardIndex = 0;
+        newCardIndex = 0
       }
     }
 
