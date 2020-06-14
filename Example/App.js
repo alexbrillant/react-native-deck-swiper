0a507becfd054a2bcd8d0f519bd4f727965f87b3
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2020-06-14 16:56:20 +0200

    Update example to be more meaningful and realistic

--- Example/App.js
+++ Example/App.js
@@ -3,20 +3,24 @@ import Swiper from 'react-native-deck-swiper'
 import { Button, StyleSheet, Text, View } from 'react-native'
 
 // demo purposes only
-function * range (start, end) {
-  for (let i = start; i <= end; i++) {
-    yield i
+let fakeFetchIndex = 0
+
+async function fetchCardsAsync() {
+  // You would replace this with a call to fetch()
+  await new Promise(r => setTimeout(r, 500))
+  console.log("Finished data")
+  items = []
+  for(let i = 0; i < 10; i++, fakeFetchIndex++) {
+    items.push("Card " + fakeFetchIndex)
   }
+  return items
 }
 
 export default class Exemple extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      cards: [...range(1, 50)],
-      swipedAllCards: false,
-      swipeDirection: '',
-      cardIndex: 0
+      cards: ["Default"],
     }
   }
 
@@ -28,19 +32,32 @@ export default class Exemple extends Component {
     )
   };
 
-  onSwiped = (type) => {
-    console.log(`on swiped ${type}`)
+  onSwiped = (type, cardIndex) => {
+    console.log(`on swiped ${type}, index ${cardIndex}`)
+  }
+
+  onTapCard = (cardIndex) => {
+    console.log(`on tap ${cardIndex}`)
+    this.swiper.swipeLeft()
+  }
+
+  fetchNewCards() {
+    fetchCardsAsync().then((items) => {
+      this.setState({
+        cards: items
+      })
+    })
+  }
+
+  componentDidMount() {
+    // Fetch new cards when the app starts
+    this.fetchNewCards()
   }
 
   onSwipedAllCards = () => {
-    this.setState({
-      swipedAllCards: true
-    })
-  };
-
-  swipeLeft = () => {
-    this.swiper.swipeLeft()
-  };
+    // Fetch new cards when everything has been swiped
+    this.fetchNewCards()
+  }
 
   render () {
     return (
@@ -49,14 +66,13 @@ export default class Exemple extends Component {
           ref={swiper => {
             this.swiper = swiper
           }}
-          onSwiped={() => this.onSwiped('general')}
-          onSwipedLeft={() => this.onSwiped('left')}
-          onSwipedRight={() => this.onSwiped('right')}
-          onSwipedTop={() => this.onSwiped('top')}
-          onSwipedBottom={() => this.onSwiped('bottom')}
-          onTapCard={this.swipeLeft}
+          onSwiped={(cardIndex) => this.onSwiped('general', cardIndex)}
+          onSwipedLeft={(cardIndex) => this.onSwiped('left', cardIndex)}
+          onSwipedRight={(cardIndex) => this.onSwiped('right', cardIndex)}
+          onSwipedTop={(cardIndex) => this.onSwiped('top', cardIndex)}
+          onSwipedBottom={(cardIndex) => this.onSwiped('bottom', cardIndex)}
+          onTapCard={(cardIndex) => this.onTapCard(cardIndex)}
           cards={this.state.cards}
-          cardIndex={this.state.cardIndex}
           cardVerticalMargin={80}
           renderCard={this.renderCard}
           onSwipedAll={this.onSwipedAllCards}
